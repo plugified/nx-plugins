adaee3482f54b011de6139e9b204cf2cedcac606
Pradeep Chauhan <pradeepchauhan91@gmail.com>
2020-11-02 03:11:40 +0530

    fixed coverage not created

--- packages/nx-fastify/jest.config.js
+++ packages/nx-fastify/jest.config.js
@@ -10,5 +10,7 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  verbose: true,
+  collectCoverage: true,
   coverageDirectory: '../../coverage/packages/nx-fastify',
 };
